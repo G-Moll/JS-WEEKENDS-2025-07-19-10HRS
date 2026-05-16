0fed6baada445f8ca3a84b69ac1f3358b6f5c5a1
RDK <photocromatic@gmail.com>
2026-05-16 13:31:51 -0600

    ws

--- LABS-JS-S01-PT-03/02-PROJECTS/LAB-02-GEO-FAVS/frontend/js/app.core/CameraCore.js
+++ LABS-JS-S01-PT-03/02-PROJECTS/LAB-02-GEO-FAVS/frontend/js/app.core/CameraCore.js
@@ -1,12 +1,28 @@
 import { CameraManager  } from "../camera/CameraManager.js";
 
 class CameraCore {
+
+    static camera = null;
+
     constructor() {}
 
     static init() {
-        console.log( "CameraCore Initialized..!" );
-        // new CameraManager();
+        this.camera = new CameraManager( "cameraPreview" );
+
+        document.getElementById( "startCam" ).addEventListener( "click", CameraCore.startCamHandler );
+        document.getElementById( "stopCam" ).addEventListener( "click", CameraCore.stopCamHandler );
     }
+
+    static startCamHandler( e ) {
+        console.log( this );
+        CameraCore.camera.start();
+    }
+
+    static stopCamHandler( e ) {
+        console.log( this );
+        CameraCore.camera.stop();
+    }
+
 }
 
 export { CameraCore };
